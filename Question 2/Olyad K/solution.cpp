43b891937dd7b23b4b2e7e212c8a31bdfb40b267
Olyad Kitila <olyad987@gmail.com>
2021-03-15 02:14:54 -0400

    Add files via upload

---
+++ Question 2/Olyad K/solution.cpp
@@ -0,0 +1,26 @@
+//
+//  solution.cpp
+//  minOperations
+//
+//  Created by Olyad Kitila on 3/15/21.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> minOperations(string boxes) {
+    vector<int> answer;
+    
+    for (int i = 0; i < boxes.length(); i++) {
+        int moves = 0;
+        
+        for (int j = 0; j < boxes.length(); j++)
+            if ((boxes[j] - '0') == 1)
+                moves += abs(i - j);
+        
+        answer.push_back(moves);
+    }
+    
+    return answer;
+}
