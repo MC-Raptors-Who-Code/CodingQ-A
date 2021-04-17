1ea9fa247ec0e4242b1c618fb2b60c94d5a1ae34
rosegriffin <60629645+rosegriffin@users.noreply.github.com>
2021-04-17 18:39:55 -0400

    Update Solution.java

--- Question 4/Rose G/Solution.java
+++ Question 4/Rose G/Solution.java
@@ -2,7 +2,7 @@ class Solution {
     public int numJewelsInStones(String jewels, String stones) {
 		int count = 0;
 		for (int i = 0; i < stones.length(); i++) {
-      //Check if the character at the current index is in jewels
+            		//Check if the character at the current index is in jewels
 			if (jewels.indexOf(stones.charAt(i)) != -1)
 				count++;
 		}
